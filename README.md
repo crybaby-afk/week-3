36fb72ee51b23d6c3031b86b45d8e160ac60bcea
crybaby-afk <toolarry55@gmail.com>
2024-11-19 11:53:33 +0300

    correction

---
+++ README.md
@@ -0,0 +1,20 @@
+# Flatdango Movie Theater
+
+## Overview
+Flatdango Movie Theater is a simple web application that allows users to view a list of movies, see details such as the movie poster, description, and available tickets, and purchase tickets. When a user buys a ticket, the number of available tickets is reduced, and the button is disabled if there are no more tickets available.
+
+## Features
+- View a list of movies in the sidebar.
+- See movie details such as poster, description, available tickets, and more.
+- Purchase tickets, which decreases the number of available tickets.
+- Movies are fetched from a `db.json` file (can be served using `JSON Server`).
+
+## Tech Stack
+- HTML
+- CSS
+- JavaScript
+- JSON (mock database)
+
+## Installation
+
+
---
+++ index.js
@@ -0,0 +1,77 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const filmsList = document.getElementById("films");
+  const posterImg = document.getElementById("poster");
+  const titleElement = document.getElementById("title");
+  const runtimeElement = document.getElementById("runtime");
+  const showtimeElement = document.getElementById("showtime");
+  const availableTicketsElement = document.getElementById("available-tickets");
+  const descriptionElement = document.getElementById("description"); 
+  const buyTicketButton = document.getElementById("buy-ticket");
+
+  fetch("http://localhost:3000/films")
+      .then(response => response.json())
+      .then(data => {
+          data.forEach(film => {
+              const filmItem = document.createElement("li");
+              filmItem.className = "film item";
+              filmItem.innerText = film.title;
+              filmItem.dataset.id = film.id; 
+              filmsList.appendChild(filmItem);
+
+              filmItem.addEventListener("click", () => {
+                  displayFilmDetails(film);
+              });
+          });
+
+          if (data.length > 0) {
+              displayFilmDetails(data[0]);
+          }
+      })
+      .catch(error => console.error("Error fetching films:", error));
+
+  function displayFilmDetails(film) {
+      posterImg.src = film.poster;
+      titleElement.innerText = film.title;
+      runtimeElement.innerText = `Runtime: ${film.runtime}`;
+      showtimeElement.innerText = `Showtime: ${film.showtime}`;
+      const availableTickets = film.capacity - film.tickets_sold;
+      availableTicketsElement.innerText = `Available Tickets: ${availableTickets}`;
+      descriptionElement.innerText = film.description; 
+
+      if (availableTickets > 0) {
+          buyTicketButton.disabled = false;
+          buyTicketButton.onclick = () => buyTicket(film);
+      } else {
+          buyTicketButton.disabled = true;
+      }
+  }
+
+  function buyTicket(film) {
+      const availableTickets = film.capacity - film.tickets_sold;
+      if (availableTickets > 0) {
+          film.tickets_sold += 1;
+          const newAvailableTickets = film.capacity - film.tickets_sold;
+          availableTicketsElement.innerText = `Available Tickets: ${newAvailableTickets}`;
+          
+          // Update the database
+          fetch(`http://localhost:3000/films/${film.id}`, {
+              method: 'PUT',
+              headers: {
+                  'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({ tickets_sold: film.tickets_sold }),
+          })
+          .then(response => response.json())
+          .then(updatedFilm => {
+              // Optionally update UI or do something with the updated film
+          })
+          .catch(error => console.error("Error updating tickets:", error));
+
+          if (newAvailableTickets === 0) {
+              buyTicketButton.disabled = true;
+          }
+      }
+  }
+});
+
+
